e9268e9ed8b386f84f1382a9b913140a46ad408d
Glenn <glennchiang24@gmail.com>
2025-10-25 00:32:29 +0800

    Add input fields

--- src/pages/auth/AuthLayout.tsx
+++ src/pages/auth/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@tanstack/react-router";
 
 export default function AuthLayout() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
+    <div className="flex flex-col gap-10 justify-center items-center w-screen h-screen">
       <Outlet/>
     </div>
   )
--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,7 +1,24 @@
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
 export default function SignUp() {
   return (
     <>
       <h1>Sign Up</h1>
+      <FieldGroup className="w-[426px]">
+        <Field>
+          <FieldLabel htmlFor="username"><h2>Username</h2></FieldLabel>
+          <Input id="username" autoComplete="off"/>
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="email"><h2>Email</h2></FieldLabel>
+          <Input id="email" autoComplete="off"/>
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="password"><h2>Password</h2></FieldLabel>
+          <Input id="password" autoComplete="off"/>
+        </Field>
+      </FieldGroup>
     </>
   )
 }
