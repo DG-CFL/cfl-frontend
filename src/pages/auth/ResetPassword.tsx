a4926f51eacc9e9b6f5ba64616794e2aab3fcf9b
Glenn <glennchiang24@gmail.com>
2025-10-25 17:22:06 +0800

    Add ResetPassword page

--- src/pages/auth/ResetPassword.tsx
+++ src/pages/auth/ResetPassword.tsx
@@ -1,5 +1,28 @@
+import logo from '@/assets/cfl-logo.png'
+import { Button } from '@/components/ui/button'
+import { Field, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { MaskableInput } from '@/components/ui_custom/MaskableInput'
+
 export default function ResetPassword() {
-  (
-    <></>
+  return (
+    <div className="space-y-10 flex flex-col justify-center items-center">
+      <img src={logo} alt="CFL Logo" />
+      <h1>Reset Password</h1>
+      <FieldGroup>
+        <Field>
+          <FieldLabel htmlFor="new-password">
+            <h3>New password</h3>
+          </FieldLabel>
+          <MaskableInput id="new-password" />
+        </Field>
+        <Field>
+          <FieldLabel htmlFor="confirm-password">
+            <h3>Re-enter password</h3>
+          </FieldLabel>
+          <MaskableInput id="confirm-password" />
+        </Field>
+        <Button>Reset</Button>
+      </FieldGroup>
+    </div>
   )
 }
