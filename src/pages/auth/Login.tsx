9fc460ad1d4032cb017ca1430e6dc5c5a7cf5b3c
Glenn <glennchiang24@gmail.com>
2025-10-25 22:05:49 +0800

    Build Login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,3 +1,44 @@
+import { Button } from '@/components/ui/button'
+import { Checkbox } from '@/components/ui/checkbox'
+import { Field, FieldLabel } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { MaskableInput } from '@/components/ui_custom/MaskableInput'
+import { Link } from '@tanstack/react-router'
+
 export default function Login() {
-  return (<></>)
+  return (
+    <div className="w-3/4 m-auto flex flex-col items-center gap-8 ">
+      <h1>Login</h1>
+      <Field>
+        <FieldLabel htmlFor="email">
+          <h3>Email</h3>
+        </FieldLabel>
+        <Input id="email" />
+      </Field>
+      <Field>
+        <FieldLabel htmlFor="password">
+          <h3>Password</h3>
+        </FieldLabel>
+        <MaskableInput id="password" />
+      </Field>
+      <div className="w-full flex justify-between">
+        <div className="flex items-center gap-3">
+          <Checkbox id="remember-me" />
+          <Label
+            htmlFor="remember-me"
+            className="text-muted-foreground text-lg"
+          >
+            Remember me
+          </Label>
+        </div>
+        <Link to="/forgot-password">Forgot Password</Link>
+      </div>
+      <Button className='w-full'>Login</Button>
+      <div className="w-full flex justify-between">
+        <span className="text-muted-foreground text-lg">Don't have an account?</span>
+        <Link to="/sign-up">Sign up now</Link>
+      </div>
+    </div>
+  )
 }
