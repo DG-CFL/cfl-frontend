224337b650760269cc9bb6c35fe352332e89baa3
Glenn <glennchiang24@gmail.com>
2025-11-12 22:51:50 +0800

    Extract volunteer list data

---
+++ src/types/vms.tsx
@@ -0,0 +1,12 @@
+export type Volunteer = {
+  id: string
+  name : string
+  age : number
+  gender : "Male" | "Female"
+  language : string []
+  activities : number
+  trainings : number 
+  optin : boolean 
+  certifications : string[]
+  volunteerHistory : string
+}
