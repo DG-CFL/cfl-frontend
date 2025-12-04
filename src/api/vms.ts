65f8826d61d4c6bc778702e4fe2d30474afd0990
Glenn <glennchiang24@gmail.com>
2025-12-04 15:19:47 +0800

    Add delete volunteer endpoints

--- src/api/vms.ts
+++ src/api/vms.ts
@@ -1,10 +1,10 @@
-import type { Volunteer } from "@/types/vms";
-import { api } from "./baseApi";
+import type { Volunteer } from '@/types/vms'
+import { api } from './baseApi'
 
 // Returns the list of all volunteers
-// TODO: Allow filtering by passing in a FilterOptions object
+// TODO: Allow filtering by passing in search filters as an object
 export async function getVolunteers(): Promise<Volunteer[]> {
-  const res = await api.get("/volunteers")
+  const res = await api.get('/volunteers')
   return res.data
 }
 
@@ -12,4 +12,16 @@ export async function getVolunteers(): Promise<Volunteer[]> {
 export async function getVolunteer(volunteerId: number): Promise<Volunteer> {
   const res = await api.get(`/volunteers/${volunteerId}`)
   return res.data
+}
+
+// Deletes a volunteer specified by the volunteerId
+export async function deleteVolunteer(volunteerId: number) {
+  await api.delete(`/volunteers/${volunteerId}`)
+}
+
+// Deletes all volunteers specified by the list of volunteerIds
+export async function deleteVolunteers(volunteerIds: number[]) {
+  await api.delete('/volunteers', {
+    data: { ids: volunteerIds },
+  })
 }
