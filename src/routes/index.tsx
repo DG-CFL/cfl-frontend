7918ec889f409b7d54462ac48e54af7beea2c013
Glenn <glennchiang24@gmail.com>
2025-12-06 22:06:16 +0800

    Reorganize routes into separate folders

---
+++ src/routes/index.tsx
@@ -0,0 +1,71 @@
+import { createRootRoute, createRouter, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+import { appLayoutRoute, homeRoute } from './app'
+import {
+  createEventRoute,
+  createEventSuccessRoute,
+  editEventRoute,
+  editEventSuccessRoute,
+  eventsLayoutRoute,
+  manageEventsRoute,
+  viewEventRoute,
+} from './events'
+import { volunteerPageRoute } from './vms'
+import {
+  authLayoutRoute,
+  signUpRoute,
+  signUpSuccessRoute,
+  resetPasswordRoute,
+  resetPasswordSuccessRoute,
+  resetPasswordEmailRoute,
+  authSplitLayoutRoute,
+  loginRoute,
+  forgotPasswordRoute,
+} from './auth'
+
+export const rootRoute = createRootRoute({
+  component: () => (
+    <div className="flex">
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  ),
+})
+
+const routeTree = rootRoute.addChildren([
+  appLayoutRoute.addChildren([
+    homeRoute,
+    eventsLayoutRoute.addChildren([
+      manageEventsRoute,
+      viewEventRoute,
+      createEventRoute,
+      editEventRoute,
+      createEventSuccessRoute,
+      editEventSuccessRoute,
+    ]),
+    volunteerPageRoute,
+  ]),
+  authLayoutRoute.addChildren([
+    signUpRoute,
+    signUpSuccessRoute,
+    resetPasswordRoute,
+    resetPasswordSuccessRoute,
+    resetPasswordEmailRoute,
+    authSplitLayoutRoute.addChildren([loginRoute, forgotPasswordRoute]),
+  ]),
+])
+
+export const router = createRouter({
+  routeTree,
+  context: {},
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
