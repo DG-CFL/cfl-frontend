25cf2d530c4ee1fde01d7c75b6dcf618bb77b15d
Glenn <glennchiang24@gmail.com>
2025-12-07 11:30:24 +0800

    Add support for query params

--- src/api/vms.ts
+++ src/api/vms.ts
@@ -1,10 +1,20 @@
-import type { Volunteer, VolunteerCertification, VolunteerEvent } from '@/types/vms'
+import type {
+  Volunteer,
+  VolunteerCertification,
+  VolunteerEvent,
+} from '@/types/vms'
 import { api } from './baseApi'
 
+import { serializeParams, type QueryParams } from './queryUtils'
+
 // Returns a list of volunteers
-// TODO: Accept params for filtering, sorting and pagination
-export async function getVolunteers(): Promise<Volunteer[]> {
-  const res = await api.get('/volunteers')
+export async function getVolunteers(
+  params: QueryParams = {},
+): Promise<Volunteer[]> {
+  const res = await api.get('/volunteers', {
+    params,
+    paramsSerializer: serializeParams,
+  })
   return res.data
 }
 
@@ -15,13 +25,17 @@ export async function getVolunteer(volunteerId: number): Promise<Volunteer> {
 }
 
 // Returns the list of the specified volunteer's certifications
-export async function getVolunteerCertifications(volunteerId: number): Promise<VolunteerCertification> {
+export async function getVolunteerCertifications(
+  volunteerId: number,
+): Promise<VolunteerCertification> {
   const res = await api.get(`/volunteers/${volunteerId}/certifications`)
   return res.data
 }
 
 // Returns the list of events attended by the specified volunteer
-export async function getVolunteerHistory(volunteerId: number): Promise<VolunteerEvent> {
+export async function getVolunteerHistory(
+  volunteerId: number,
+): Promise<VolunteerEvent> {
   const res = await api.get(`/volunteers/${volunteerId}/history`)
   return res.data
 }
