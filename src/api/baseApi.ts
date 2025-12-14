6895e8324621296b6b023601f939cf8e2faa5607
Glenn <glennchiang24@gmail.com>
2025-12-14 12:06:56 +0800

    Implement camelcase to snakecase conversion for request body

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,5 +1,6 @@
-import axios from 'axios'
+import axios, { type AxiosRequestConfig } from 'axios'
 import { getAuth } from 'firebase/auth'
+import { camelToSnake, snakeToCamel } from './utils/utils'
 
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL + "/api",
@@ -16,3 +17,24 @@ api.interceptors.request.use(async (config) => {
   }
   return config
 })
+
+// Convert request body and params from camel case to snake case
+api.interceptors.request.use((config) => {
+    if (config.data && typeof config.data === 'object') {
+        config.data = camelToSnake(config.data);
+    }
+    if (config.params && typeof config.params === 'object') {
+        config.params = camelToSnake(config.params);
+    }
+    return config;
+});
+
+// Convert request body from snake case to camel case
+api.interceptors.response.use(
+  (res) => {
+    if (res.data && typeof res.data === 'object') {
+      res.data = snakeToCamel(res.data)
+    } 
+    return res
+  }, error => Promise.reject(error)
+)
