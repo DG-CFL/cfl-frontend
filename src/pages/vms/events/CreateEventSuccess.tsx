d92d98879981db541801e148e97266d9868f0624
haojun-mah <mahhaojun03@gmail.com>
2026-01-14 00:34:41 +0800

    Update create event success page

--- src/pages/vms/events/CreateEventSuccess.tsx
+++ src/pages/vms/events/CreateEventSuccess.tsx
@@ -1,12 +1,21 @@
 import checkIcon from '@/assets/check-icon.svg'
+import { useNavigate } from '@tanstack/react-router'
 
 export default function CreateEventSuccess() {
+  const navigate = useNavigate()
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-4">
       <img src={checkIcon} alt="Success" />
-      <h1>Your event has been successfully created</h1>
+      <h1>Event successfully created</h1>
       <p className="text-center text-xl font-normal leading-7">
         It might take a few minutes for the changes to reflect on the main page
+      </p>
+      <p
+        className="mt-2 cursor-pointer text-xl font-semibold text-[#545f71] underline"
+        onClick={() => navigate({ to: '/events' })}
+      >
+        Back to Events Page
       </p>
     </div>
   )
