2220a05fc7ae9406a4d9b73cad76d95f6d913b87
haojun-mah <mahhaojun03@gmail.com>
2026-01-14 12:49:41 +0800

    Fix centering of create event success page

--- src/pages/vms/events/CreateEventSuccess.tsx
+++ src/pages/vms/events/CreateEventSuccess.tsx
@@ -5,7 +5,7 @@ export default function CreateEventSuccess() {
   const navigate = useNavigate()
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4">
+    <div className="mx-auto flex min-h-screen w-[calc(100vw-250px)] flex-col items-center justify-center gap-4 text-center">
       <img src={checkIcon} alt="Success" />
       <h1>Event successfully created</h1>
       <p className="text-center text-xl font-normal leading-7">
